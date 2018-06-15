fe14b41b8c3a5980452ac1df0cb266030372c768
JunqiZhang <377964854@qq.com>
2018-06-15 14:12:38 +0800

    modify one file

--- source/ELVISIII Example - AIO/main.c
+++ source/ELVISIII Example - AIO/main.c
@@ -3,10 +3,6 @@
  * National Instruments Corporation.
  * All rights reserved.
  *
-=======
- */
-/**
->>>>>>> master
  * Overview:
  * Demonstrates using the analog input and output (AIO).
  * Writes an initial value on connector A.Reads the write value from connector A.
@@ -38,9 +34,6 @@
 
 #if !defined(LoopDuration)
 #define LoopDuration    60  // How long to output the signal, in seconds 
-=======
-#define LoopDuration    60  /* How long to output the signal, in seconds */
->>>>>>> master
 #endif
 
 extern ELVISIII_Ai connector_A;
@@ -59,13 +52,6 @@ int main(int argc, char **argv)
     // Open the ELVIS III NiFpga Session.
     // This function MUST be called before all other functions. After this call
     // is complete the ELVIS III target will be ready to be used.
-=======
-    /*
-     * Open the ELVIS III NiFpga Session.
-     * This function MUST be called before all other functions. After this call
-     * is complete the ELVIS III target will be ready to be used.
-     */
->>>>>>> master
     status = NiELVISIIIv10_Open();
     if (NiELVISIIIv10_IsNotSuccess(status))
     {
@@ -114,76 +100,6 @@ int main(int argc, char **argv)
     // Normally, the main function runs a long running or infinite loop.
     // Keep the program running so that you can measure the output using
     // an external instrument.
-=======
-    /*
-     * Write the value to A/AO0 to set it to the desired value.
-     * The channel is on connector A.
-     */
-    Aio_Write(&Ao, 3, AO_A0_VAL);
-
-    /*
-     * Set the number of valid channels on connector A.
-     */
-    Ai_Counter(&connector_A, 12);
-
-    /*
-     * Configure the range of the analog input channel on connector A.
-     */
-    Ai_Configure(&connector_A, Ai_Channel0, Ai_Range0);
-
-    /*
-     * Configure the divisor for the analog sample rate on connector A.
-     */
-    Ai_Divisor(&connector_A, 40000000, 1000);
-
-    /*
-     * Sleep before reading.Wait for the value writing to the register.
-     */
-    sleep (1);
-
-    /*
-     * Read from AI channel A/AI0.
-     * The channel is on connector A.
-     */
-    double ai_A0 = Aio_Read(&connector_A, Ai_Channel0);
-
-    /*
-     * Print out the desired logic level of A/AI0.
-     */
-    printf("ai_A0   = %f\n", ai_A0);
-
-    /*
-     * Set the number of valid channels on connector B.
-     */
-    Ai_Counter(&connector_B, 12);
-
-    /*
-     * Configure the range of the analog input channel on connector B.
-     */
-    Ai_Configure(&connector_B, Ai_Channel1_5, Ai_Range1);
-
-    /*
-     * Configure the divisor for the analog sample rate on connector B.
-     */
-    Ai_Divisor(&connector_B, 40000000, 1000);
-
-    /*
-     * Read from AI channel B/AI1 and B/AI5.
-     * Each channel is on connector B.
-     */
-    double ai_B1_5 = Aio_Read(&connector_B, Ai_Channel1_5);
-
-    /*
-     * Print the difference of the values in two channels.
-     */
-    printf("ai_B1_5 = %f\n", ai_B1_5);
-
-    /*
-     * Normally, the main function runs a long running or infinite loop.
-     * Keep the program running so that you can measure the output using
-     * an external instrument.
-     */
->>>>>>> master
     time(&currentTime);
     finalTime = currentTime + LoopDuration;
     while (currentTime < finalTime)
@@ -196,16 +112,5 @@ int main(int argc, char **argv)
     status = NiELVISIIIv10_Close();
 
     // Returns 0 if successful.
-=======
-    /*
-     * Close the ELVISIII NiFpga Session.
-     * This function MUST be called after all other functions.
-     */
-    status = NiELVISIIIv10_Close();
-
-    /*
-     * Returns 0 if successful.
-     */
->>>>>>> master
     return status;
 }
