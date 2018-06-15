51e5c3947af8fe752c5e6aad259b32d7f09911be
JunqiZhang <377964854@qq.com>
2018-06-15 10:32:14 +0800

    Rename Function

--- source/ELVISIII Example - AIO - N Sample/AIO_N_Sample.h
+++ source/ELVISIII Example - AIO - N Sample/AIO_N_Sample.h
@@ -19,11 +19,11 @@
 //AI channel selection number (DIFF mode)
 #define DIFF_NUM 4
 
-//AI_Fxp
+//AI Fixed-point
 #define AI_WordLength 24
 #define AI_IntegerWordLength 5
 
-//AO_Fxp
+//AO Fixed-point
 #define AO_WordLength 20
 #define AO_IntegerWordLength 5
 
@@ -35,24 +35,25 @@
 extern "C" {
 #endif
 
-//AO FIFO
+//AO FIFO Fixed-point
 typedef enum
 {
-	HostToTarget_FIFO_FXP_B = 4,
-	HostToTarget_FIFO_FXP_A = 5,
+    HostToTarget_FIFO_FXP_B = 4,
+    HostToTarget_FIFO_FXP_A = 5,
 } HostToTarget_FIFO_FXP;
 
-
-//AI FIFO
+//AI FIFO Fixed-point
 typedef enum
 {
-	TargetToHost_FIFO_FXP_B = 6,
-	TargetToHost_FIFO_FXP_A = 7,
+    TargetToHost_FIFO_FXP_B = 6,
+    TargetToHost_FIFO_FXP_A = 7,
 } TargetToHost_FIFO_FXP;
 
-
 /**
- * Specify which AI channel to configure.
+ * Specify the numbers of AI channels.
+ * The analog input channels have RSE mode and DIFF mode.
+ * RSE mode corresponding to one particular analog input channel.
+ * DIFF mode corresponding to the difference of two analog input channels.
  *
  * RSE (the fourth bit is 1):
  * Ai_Channel 0 : 1 000b
@@ -68,33 +69,29 @@ typedef enum
  */
 typedef enum
 {
-	//RSE mode
-	Ai_Channel0 = 0x08,
-	Ai_Channel1 = 0x09,
-	Ai_Channel2 = 0x0a,
-	Ai_Channel3 = 0x0b,
-	Ai_Channel4 = 0x0c,
-	Ai_Channel5 = 0x0d,
-	Ai_Channel6 = 0x0e,
-	Ai_Channel7 = 0x0f,
+    //RSE mode
+    Ai_Channel0 = 0x08,
+    Ai_Channel1 = 0x09,
+    Ai_Channel2 = 0x0a,
+    Ai_Channel3 = 0x0b,
+    Ai_Channel4 = 0x0c,
+    Ai_Channel5 = 0x0d,
+    Ai_Channel6 = 0x0e,
+    Ai_Channel7 = 0x0f,
 
-	//DIFF mode
-	Ai_Channel0_4 = 0x00,
-	Ai_Channel1_5 = 0x01,
-	Ai_Channel2_6 = 0x02,
-	Ai_Channel3_7 = 0x03,
+    //DIFF mode
+    Ai_Channel0_4 = 0x00,
+    Ai_Channel1_5 = 0x01,
+    Ai_Channel2_6 = 0x02,
+    Ai_Channel3_7 = 0x03,
 } Ai_Channel;
 
-
-/**
- * Specify which AO channel to configure.
- */
+// Specify the numbers of AO channels.
 typedef enum
 {
-	Ao_Channel0 = 0b01,
-	Ao_Channel1 = 0b10,
+    Ao_Channel0 = 0b01,
+    Ao_Channel1 = 0b10,
 }Ao_Channel;
-
 
 /**
  * Specify the voltage range of the AI channel.
@@ -106,103 +103,70 @@ typedef enum
  */
 typedef enum
 {
-	Ai_Range0 = 0x00,
-	Ai_Range1 = 0x10,
-	Ai_Range2 = 0x20,
-	Ai_Range3 = 0x30,
+    Ai_Range0 = 0x00,
+    Ai_Range1 = 0x10,
+    Ai_Range2 = 0x20,
+    Ai_Range3 = 0x30,
 } Ai_Range;
 
-
-/**
- * Registers and settings for a particular AIO N Sample.
- * Explanation of each Registers is written below.
- */
+// Registers for a particular AIO N Sample.
+// Analog input and analog output of N Sample share the same structure, but it is
+// not possible to read from an analog output and write to an analog input.
 typedef struct
 {
-	uint32_t cnfg;                          /**< AI Configuration Register */
+    uint32_t cnfg;                          // AI Configuration Register 
 
-	uint32_t ai_cntr;                       /**< AI Divisor Register */
-	uint32_t ao_cntr;                       /**< AO Divisor Register */
+    uint32_t ai_cntr;                       // AI Divisor Register 
+    uint32_t ao_cntr;                       // AO Divisor Register 
 
-	uint32_t cnt;                           /**< AI Counter Register */
+    uint32_t cnt;                           // AI Counter Register 
 
-	uint32_t ai_enable;                     /**< AI DMA Enable Register */
-	uint32_t ao_enable;                     /**< AO DMA Enable Register */
+    uint32_t ai_enable;                     // AI DMA Enable Register 
+    uint32_t ao_enable;                     // AO DMA Enable Register 
 } ELVISIII_Aio;
 
+// Set the number of valid analog input channels.
+void Ai_Counter(ELVISIII_Aio* bank, uint8_t counter);
 
-/**
- * Set the AI Counter Register.
- */
-void Ai_Counter(ELVISIII_Aio* connector, uint8_t counter);
+// Set the AI configuration options.
+void Ai_Configure(ELVISIII_Aio* bank, Ai_Channel channel, Ai_Range range);
 
+// Generate the divisor for the AI sample rate.
+void Ai_Divisor(ELVISIII_Aio* bank, uint32_t ClockRate, uint32_t SampleRate);
 
-/**
- * Set the AI Configuration Register.
- */
-void Ai_Configure(ELVISIII_Aio* connector, Ai_Channel channel, Ai_Range range);
+// Set the DMA Enable Flag for one bank.
+void Ai_Enable(ELVISIII_Aio* bank);
 
+// Read groups of AI values as an AI FIFO from a single channel.
+void Ai_ReadFifo(ELVISIII_Aio*                bank,
+                 TargetToHost_FIFO_FXP         fifo,
+                  uint64_t*                    fxp_buffer_receive,
+                  size_t                       fifo_size,
+                  uint32_t                      timeout,
+                  size_t*                      elementsRemaining);
 
-/**
- * Set the AI Divisor Register.
- */
-void Ai_Divisor(ELVISIII_Aio* connector, uint32_t ClockRate, uint32_t SampleRate);
+// Convert fixed-point values of the FIFO to double values.
+void ConvertUint64ArrayToDoubleArray(uint64_t *fxp_buffer_receive, size_t fifo_size, double *value);
 
+// Generate the divisor for the AO sample rate.
+void Ao_Divisor(ELVISIII_Aio* bank, uint32_t ClockRate, uint32_t SampleRate);
 
-/**
- * Set the AI DMA Enable Register.
- */
-void Ai_Enable(ELVISIII_Aio* connector);
+// Set the DMA Enable value for an analog output channel.
+void Ao_Enable(ELVISIII_Aio* bank, Ao_Channel channel);
 
+// Convert double values to fixed-point values of the FIFO.
+void ConvertDoubleArrayToUint64Array(double *value, uint64_t *fxp_buffer_send, size_t fifo_size);
 
-/**
- * Read groups of AI values from a AI FIFO.
- */
-void Ai_ReadFifo(ELVISIII_Aio*         connector,
-		         TargetToHost_FIFO_FXP fifo,
-			 	 uint64_t*             fxp_buffer_recv,
-			 	 size_t                fifo_size,
-			 	 uint32_t              timeout,
-			 	 size_t*               elementsRemaining);
-
-
-/**
- * Get groups of values from one channel.
- */
-void Ai_GetVal(uint64_t *fxp_buffer_recv, size_t fifo_size, double *val);
-
-
-/**
- * Set the AO Divisor Register.
- */
-void Ao_Divisor(ELVISIII_Aio* connector, uint32_t ClockRate, uint32_t SampleRate);
-
-
-/**
- * Set the AO DMA Enable Register.
- */
-void Ao_Enable(ELVISIII_Aio* connector, Ao_Channel channel);
-
-
-/**
- * Turn groups of values from double to Fix Point.
- */
-void Ao_SetVal(double *val, uint64_t *fxp_buffer_send, size_t fifo_size);
-
-
-/**
- * Write groups of AO values to a AO FIFO.
- */
-void Ao_WriteFifo(ELVISIII_Aio*         connector,
-		          HostToTarget_FIFO_FXP fifo,
-			 	  const uint64_t*       fxp_buffer_recv,
-			 	  size_t                fifo_size,
-			 	  uint32_t              timeout,
-			 	  size_t*               elementsRemaining);
-
+// Write groups of AO values as an AO FIFO to a single channel.
+void Ao_WriteFifo(ELVISIII_Aio*             bank,
+                  HostToTarget_FIFO_FXP     fifo,
+                   const uint64_t*           fxp_buffer_send,
+                   size_t                    fifo_size,
+                   uint32_t                  timeout,
+                   size_t*                   elementsRemaining);
 
 #if NiFpga_Cpp
 }
 #endif
 
-#endif /* AIO_N_Sample_h_ */
+#endif // AIO_N_Sample_h_ 
--- source/ELVISIII Example - AIO - N Sample/main.c
+++ source/ELVISIII Example - AIO - N Sample/main.c
@@ -2,8 +2,33 @@
  * Copyright (c) 2018,
  * National Instruments Corporation.
  * All rights reserved.
+ *
+ * Overview:
+ * Demonstrates using the analog input and output of N Sample (AIO - N Sample).
+ * Read a group of values from one analog input channel on bank A.
+ * Write a group of values to one analog output channel on bank B.
+ *
+ * Instructions:
+ * 1. Install an oscilloprobe on one of the Function Generator on NI ELVIS III device.
+ *    Use the oscilloprobe to probe AI0 on bank A. Use AGND as ground.
+ * 2. Install an oscilloprobe on one of the Oscilloscope on NI ELVIS III device.
+ *    Use the oscilloprobe to probe AO0 on bank B. Use AGND as ground.
+ * 3. Open "Measurements Live" (measurementslive.ni.com) and connect to the NI ELVIS III device.
+ * 4. In Instruments tab, open Function and Arbitrary Waveform Generator.
+ *       Set a square wave in 500Hz, 4Vpp and 0V DC offset.
+ * 5. In Instruments tab, open Oscilloscope.
+ *       Set Normal mode, analog edge type and 1V level.
+ * 6. Run this program.
+ *
+ * Output:
+ * The program reads groups of values from AI0 on bank A.
+ * The program writes groups of initial values to AO0 on bank B.
+ * The output is maintained for 60 s.
+ * The group of values reading from the AI0 is written to the console.
+ *
+ * Note:
+ * The Eclipse project defines the preprocessor symbol for the ELVIS III.
  */
-
 #include <stdio.h>
 #include <time.h>
 
@@ -11,156 +36,106 @@
 #include "NiELVISIIIv10.h"
 
 #if !defined(LoopDuration)
-#define LoopDuration    60  /* How long to output the signal, in seconds */
+#define LoopDuration    60  // How long to output the signal, in seconds 
 #endif
 
 //Default FIFO size.
 #define FIFO_SIZE 100
 
-extern ELVISIII_Aio connector_A;
-extern ELVISIII_Aio connector_B;
+extern ELVISIII_Aio bank_A;
+extern ELVISIII_Aio bank_B;
 
-/**
- * Overview:
- * Demonstrates using the analog input and output in N Sample (AIO - N Sample).
- * Read a groups of values from a AI FIFO (connector A, channel 0).
- * Write a groups of values to a AO FIFO (connector B, channel 0).
- *
- * Instructions:
- * 1. Use an oscilloprobe to connect FGen (one side) and AI0, AGND (another side) on connector A.
- * 2. Use an oscilloprobe to connect Oscilloscope (one side) and AO0, AGND (another side) on connector B.
- * 3. Open FGen and set a square wave in 500Hz, 4Vpp and 0V DC offset.
- * 4. Open Oscilloscope and set Normal mode. The Level is 1V and the Type is Analog edge.
- * 5. Run this program.
- *
- * Output:
- * The program reads groups of values from a AI FIFO in channel 0, connector A.
- * The program writes groups of values to a AO FIFO in channel 0, connector B.
- * The groups of values reading from the AI FIFO will be printed on the console.
- * The output is maintained for 60 s.
- *
- * Note:
- * The Eclipse project defines the preprocessor symbol for the ELVIS III.
- */
 int main(int argc, char **argv)
 {
-	NiFpga_Status status;
+    NiFpga_Status status;
 
-	double val[FIFO_SIZE];
-	uint64_t fxp_buffer_recv[FIFO_SIZE];
+    double value[FIFO_SIZE];
+    uint64_t fxp_buffer_receive[FIFO_SIZE];
 
-	double send[] = {2, 2, 2, 2, -2, -2, -2, -2};
-	uint64_t fxp_buffer_send[FIFO_SIZE];
+    double send[] = {2, 2, 2, 2, -2, -2, -2, -2};
+    uint64_t fxp_buffer_send[FIFO_SIZE];
 
-	time_t currentTime;
-	time_t finalTime;
+    time_t currentTime;
+    time_t finalTime;
 
-	printf("AnalogInputOutput - N Sample:\n");
+    printf("AnalogInputOutput - N Sample:\n");
 
-	/*
-	 * Open the ELVIS III NiFpga Session.
-	 * This function MUST be called before all other functions. After this call
-	 * is complete the ELVIS III target will be ready to be used.
-	 */
-	status = NiELVISIIIv10_Open();
-	if (NiELVISIIIv10_IsNotSuccess(status))
-	{
-	    return status;
-	}
+    // Open the ELVIS III NiFpga Session.
+    // This function MUST be called before all other functions. After this call
+    // is complete the ELVIS III target will be ready to be used.
+    status = NiELVISIIIv10_Open();
+    if (NiELVISIIIv10_IsNotSuccess(status))
+    {
+        return status;
+    }
 
-	/*
-	 * Set the AI Counter Register.
-	 */
-	Ai_Counter(&connector_A, 1);
+    // Set the number of valid channels on bank A.
+    Ai_Counter(&bank_A, 1);
 
-	/*
-	 * Set the AI Configuration Register.
-	 */
-	Ai_Configure(&connector_A, Ai_Channel0, Ai_Range0);
+    // Configure the range of the AI channel on bank A.
+    Ai_Configure(&bank_A, Ai_Channel0, Ai_Range0);
 
-	/*
-	 * Set the AI Divisor Register.
-	 */
-	Ai_Divisor(&connector_A, 40000000, 1000);
+    // Configure the divisor for the AI sample rate on bank A.
+    Ai_Divisor(&bank_A, 40000000, 1000);
 
-	/*
-	 * Set the AI DMA Enable Register.
-	 */
-	Ai_Enable(&connector_A);
+    // Set the DMA Enable Flag for bank A.
+    Ai_Enable(&bank_A);
 
-	/*
-	 * Read FXP from a AI FIFO.
-	 */
-	Ai_ReadFifo(&connector_A,
-			    TargetToHost_FIFO_FXP_A,
-				fxp_buffer_recv,
-				FIFO_SIZE,
-				NiFpga_InfiniteTimeout,
-				NULL);
+    // Read fixed-point values from an AI FIFO on bank A.
+    Ai_ReadFifo(&bank_A,
+                TargetToHost_FIFO_FXP_A,
+                fxp_buffer_receive,
+                FIFO_SIZE,
+                NiFpga_InfiniteTimeout,
+                NULL);
 
-	/*
-	 * Get groups of values from one channel.
-	 */
-	Ai_GetVal(fxp_buffer_recv, FIFO_SIZE, val);
+    // Convert fixed-point values of the FIFO to double values.
+    // The fixed-point value is an unsigned long long int value.
+    ConvertUint64ArrayToDoubleArray(fxp_buffer_receive, FIFO_SIZE, value);
 
-	/*
-	 * Print the values of AIO0.
-	 */
-	printf("Channel%d:\n", Ai_Channel0 - RSE_NUM);
-	int i;
-	for (i = 0; i < FIFO_SIZE; ++i)
-	{
-		printf("%f ", val[i]);
-		if ((i + 1) % 10 == 0)
-			printf("\n");
-	}
-	printf("\n");
+    // Print out the values of A/AI0.
+    printf("Channel%d:\n", Ai_Channel0 - RSE_NUM);
+    int i;
+    for (i = 0; i < FIFO_SIZE; ++i)
+    {
+        printf("%f ", value[i]);
+        if ((i + 1) % 10 == 0)
+            printf("\n");
+    }
+    printf("\n");
 
-	/*
-	 * Set the AO Divisor Register.
-	 */
-	Ao_Divisor(&connector_B, 40000000, 1000);
+    // Configure the divisor for the AO sample rate on bank B.
+    Ao_Divisor(&bank_B, 40000000, 1000);
 
-	/**
-	 * Set the AO DMA Enable Register
-	 */
-	Ao_Enable(&connector_B, Ao_Channel0);
+    // Set the DMA Enable Flag for AO0 on bank B.
+    Ao_Enable(&bank_B, Ao_Channel0);
 
-	/*
-	 * Turn groups of values from double to Fix Point.
-	 */
-	Ao_SetVal(send, fxp_buffer_send, sizeof(send)/sizeof(uint64_t));
+    // Convert double values to fixed-point values of the FIFO.
+    // The fixed-point value is an unsigned long long int value.
+    ConvertDoubleArrayToUint64Array(send, fxp_buffer_send, sizeof(send)/sizeof(uint64_t));
 
-	/*
-	 * Read FXP from a AI FIFO.
-	 */
-	Ao_WriteFifo(&connector_B,
-				 HostToTarget_FIFO_FXP_B,
-				 fxp_buffer_send,
-				 (sizeof(fxp_buffer_send)/sizeof(uint64_t)),
-				 NiFpga_InfiniteTimeout,
-				 NULL);
+    // Write fixed-point values to an AO FIFO on bank B.
+    Ao_WriteFifo(&bank_B,
+                 HostToTarget_FIFO_FXP_B,
+                 fxp_buffer_send,
+                 (sizeof(fxp_buffer_send)/sizeof(uint64_t)),
+                 NiFpga_InfiniteTimeout,
+                 NULL);
 
-	/*
-	 * Normally, the main function runs a long running or infinite loop.
-	 * Keep the program running so that you can measure the output using
-	 * an external instrument.
-	 */
-	time(&currentTime);
-	finalTime = currentTime + LoopDuration;
-	while (currentTime < finalTime)
-	{
-		time(&currentTime);
-	}
+    // Normally, the main function runs a long running or infinite loop.
+    // Keep the program running so that you can measure the output using
+    // an external instrument.
+    time(&currentTime);
+    finalTime = currentTime + LoopDuration;
+    while (currentTime < finalTime)
+    {
+        time(&currentTime);
+    }
 
-	/*
-	 * Close the ELVISIII NiFpga Session.
-	 * This function MUST be called after all other functions.
-	 */
-	status = NiELVISIIIv10_Close();
+    // Close the ELVISIII NiFpga Session.
+    // This function MUST be called after all other functions.
+    status = NiELVISIIIv10_Close();
 
-	/*
-	 * Returns 0 if successful.
-	 */
-	return status;
+    // Returns 0 if successful.
+    return status;
 }
